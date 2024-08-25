6385476bd01aaf5f2f1066cb40a210b96e7332e4
sameer-siddiqui <140159169+sameer-tech1240@users.noreply.github.com>
2024-08-25 11:28:53 +0530

    Delete SpringBootCrud directory

--- SpringBootCrud/SpringBootCrud/src/main/java/com/springbootcrud/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.springbootcrud.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.springbootcrud.entity.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee, Integer> {
-
-}
