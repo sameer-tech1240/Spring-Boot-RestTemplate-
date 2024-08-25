bb1d96bec64962afb2a4e53fe3128b8221ef9db6
sameer-siddiqui <140159169+sameer-tech1240@users.noreply.github.com>
2024-08-25 11:28:43 +0530

    Delete SameerSpringBoot directory

--- SameerSpringBoot/src/main/java/com/test/boot/repo/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.test.boot.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.test.boot.entity.User;
-
-public interface UserRepo extends JpaRepository<User, Integer>{
-
-}
