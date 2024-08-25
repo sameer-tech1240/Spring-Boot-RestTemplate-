dad17da2d85b805523946dbee1029b46eba590c9
sameer-siddiqui <140159169+sameer-tech1240@users.noreply.github.com>
2024-08-25 11:28:23 +0530

    Delete BookApplication-Crud directory

--- BookApplication-Crud/src/main/java/com/book/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.book.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.book.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book, Long>{
-
-}
